78e753cf273d9b88297f0db53477b8815be9b948
Nurmuhammad <98839225+nurmuhammadx@users.noreply.github.com>
2022-03-26 14:06:59 +0400

    lab2_files

---
+++ mldmlab2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Лабораторная работа 2</title>
+    <link rel="stylesheet" href="/styles/style.css">
+    <script type="text/javascript" src="/scripts/lab2.js"></script>
+</head>
+<body>
+<h1>Лабораторная работа №2</h1>
+
+<h2>Working on it!</h2>
+
+</body>
+</html>
+
+
